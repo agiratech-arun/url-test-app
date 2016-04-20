c09fce92f9dca1d034f651410ac6155967f51ca6
Arun <arun@agiratech.com>
2016-04-20 19:26:31 +0530

    url changes

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,7 @@
 class ApiController < ApplicationController
   def index
     respond_to do |format|
-      format.json { render json: {url: "http://www.flipkart.com/search?q=pendrive&sid=6bo/jdy&as=on&as-show=on&otracker=start&as-pos=1_1_ic_pen"} }
+      format.json { render json: {url: "http://www.flipkart.com/footwear/pr?p%5B%5D=facets.brand%255B%255D%3DSEVEN&sid=osp&filterNone=true&q=SEVEN&otracker=ts_seven_ImageContainer_3-0_image_Seven"} }
     end
   end
 end
