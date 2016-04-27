6fb32acbfac3d199d8e9e6242791f3d75c301432
Arun <arun@agiratech.com>
2016-04-27 14:26:40 +0530

    changes of api

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,7 @@
 class ApiController < ApplicationController
   def index
     respond_to do |format|
-      format.json { render json: {url: "http://www.flipkart.com/footwear/pr?p%5B%5D=facets.brand%255B%255D%3DSEVEN&sid=osp&filterNone=true&q=SEVEN&otracker=ts_seven_ImageContainer_3-0_image_Seven"} }
+      format.json { render json: {url: "http://www.dafiti.com.br/esporte-feminino/nike/"} }
     end
   end
 end
